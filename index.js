c59682ca47e55f69e83b8673d24f907c051f3e06
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-04-13 18:48:59 +0200

    Added container resseting capabilities. A user can also change the size of the container after being prompted for it.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const mainContainer = document.getElementById("main-container");
+
 function makeGrid(rows, columns) {
-    const mainContainer = document.getElementById("main-container");
     for (let i =0; i < rows; i++) {
         const rowDiv = document.createElement("div");
         rowDiv.className = "row";
@@ -18,4 +19,18 @@ function makeGrid(rows, columns) {
 }
 
 makeGrid(16,16)
+
+const startButton = document.createElement("button");
+
+const header = document.getElementById("header");
+
+startButton.textContent = "Open a new Container";
+startButton.addEventListener("click", () =>{
+   //ask for number of rows and columns
+    const  numberOfGrids= prompt("How many rows and columns should the new grid have?")
+    mainContainer.innerHTML = ""
+    makeGrid(numberOfGrids, numberOfGrids);
+})
+
+header.appendChild(startButton);
 
